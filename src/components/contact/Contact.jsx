0838306db51c58e60aceb92df1611540d0077ad5
npthao110 <n.p.thao.110@gmail.com>
2024-08-13 23:58:26 -0500

    Added tab icon and smptjs for receiving email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.css";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    window.Email.send({
+      SecureToken: "YOUR_SECURE_TOKEN",  
+      To: "n.p.thao.110@gmail.com",  
+      From: e.target.user_email.value,  
+      Subject: e.target.subject.value,  
+      Body: `Name: ${e.target.user_name.value}<br/> Email: ${e.target.user_email.value}<br/> Message: ${e.target.message.value}`
+    })
+      .then((message) => alert("Message sent successfully!"))
+      .catch((error) => alert("Failed to send the message, please try again."));
+  };
+
   return (
     <section className="contact container section" id="contact">
       <h2 className="section__title">Get In Touch</h2>
@@ -14,21 +30,25 @@ const Contact = () => {
           </p>
         </div>
 
-        <form action="" className="contact__form">
+        <form ref={form} onSubmit={sendEmail} className="contact__form">
           <div className="contact__form-group">
             <div className="contact__form-div">
               <input
                 type="text"
+                name="user_name"
                 className="contact__form-input"
                 placeholder="Insert your name"
+                required
               />
             </div>
 
             <div className="contact__form-div">
               <input
                 type="email"
+                name="user_email"
                 className="contact__form-input"
                 placeholder="Insert your email"
+                required
               />
             </div>
           </div>
@@ -36,23 +56,26 @@ const Contact = () => {
           <div className="contact__form-div">
             <input
               type="text"
+              name="subject"
               className="contact__form-input"
               placeholder="Insert your subject"
+              required
             />
           </div>
 
           <div className="contact__form-div contact__form-area">
             <textarea
-              name=""
+              name="message"
               id=""
               cols="30"
               rows="10"
               className="contact__form-input"
               placeholder="Write your message"
+              required
             ></textarea>
           </div>
 
-          <button className="btn">Send Message</button>
+          <button type="submit" className="btn">Send Message</button>
         </form>
       </div>
     </section>
